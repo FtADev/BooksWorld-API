c8d3e4ffc708d3e4241ddce29d16ea0097bc6c14
ftadev <akhlaghi.fatemeh@gmail.com>
2020-04-08 17:10:52 +0430

    Minor

--- src/config/DBConfig.kt
+++ src/config/DBConfig.kt
@@ -1,11 +1,5 @@
 package com.ftadev.config
 
-/**
- * User: ftadev
- * Date: 4/1/20
- * Time: 12:42 PM
- */
-
 const val dbName = ""
 const val url = "jdbc:mysql://localhost:3306/$dbName"
 const val driver = "com.mysql.jdbc.Driver"
--- src/model/BookImage.kt
+++ src/model/BookImage.kt
@@ -1,11 +1,5 @@
 package com.ftadev.model
 
-/**
- * User: ftadev
- * Date: 4/1/20
- * Time: 6:02 PM
- */
-
 data class BookImage(
     val id: Int?,
     val photo: String
